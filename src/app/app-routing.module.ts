6ddfbba4e3eb32e183aff1b5c244b2cd054f1087
Mukesh Suthar <49620706+mukeshs1510@users.noreply.github.com>
2021-05-21 23:28:00 +0530

    register and login page ui completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { RegisterpageComponent } from './components/registerpage/registerpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginpageComponent},
+  {path: "register", component: RegisterpageComponent},
+  {path: "home", component: HomepageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { RegisterpageComponent } from './components/registerpage/registerpage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    RegisterpageComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
