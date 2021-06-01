11da8f45600be14ce3c9e13ef75a1f66aab93e44
Mukesh Suthar <49620706+mukeshs1510@users.noreply.github.com>
2021-06-01 17:16:43 +0530

    Improvements

--- src/app/components/appointments/appointments.component.ts
+++ src/app/components/appointments/appointments.component.ts
@@ -13,18 +13,21 @@ export class AppointmentsComponent implements OnInit {
   hospitals: AddHospital[]
 
   hospitalName: string = ""
+  selectedValue: string = ""
 
   constructor(private firebaseService: FirebaseserviceService, private routing: Router) { }
 
   ngOnInit(): void {
     this.firebaseService.getHospitals().subscribe(hosp => {
       this.hospitals = hosp
+      console.log(this.selectedHospital)
     })
   }
 
+
   selectedHospital(id: any, hospital_name: string) {
-    this.hospitalName = hospital_name
-    console.log(hospital_name)
+    // this.hospitalName = hospital_name
+    // console.log(this.selectedHospital)
   }
 
 }
